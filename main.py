f988d123233e3fa16886029ad856221e4b3c8218
Eunseob Kim <esk223@kaist.ac.kr>
2021-03-15 14:18:00 +0900

    Constants added

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import pyautogui
+import time
+
 if __name__ == "__main__":
-    print("Hello World!")
+    # print(pyautogui.position())
+    pyautogui.PAUSE = 1
+    pyautogui.FAILSAFE = True
+
+    while True:
+        pyautogui.click(104, 594, button='left', clicks=1, interval=2)
+        print("click")
---
+++ spell_calculator.py
@@ -0,0 +1,16 @@
+GHOST = 210
+HEAL = 240
+TELEPORT = 420      # if level 1 420, else 420 - 10*level
+CLEANSE = 210
+BARRIER = 180
+IGNITE = 180
+EXHAUST = 210
+FLASH = 300
+
+BOOTS = 10.7        # skill acceleration +12
+RUNE = 15.3         # skill acceleration +18
+
+
+class SpellCalculator():
+    def __init__(self):
+        pass
